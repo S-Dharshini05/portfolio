7ef14565ff1c9016fc515fa5323887596dfd7011
Prarthana-JH <khushihiremath877@gmail.com>
2026-03-26 17:02:39 +0530

    fixed server error

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.post("/contact", (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running");
 });
